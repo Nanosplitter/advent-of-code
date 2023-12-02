0fa2cc729238079ba98008d1da4dd4b9ff6e35fe
Colin Wilson <github@colinwilson.dev>
2023-12-02 12:12:59 -0500

    Day 1 solutions

---
+++ day1/Day1.java
@@ -0,0 +1,31 @@
+package day1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("/Users/colinwilson/Documents/GitHub/advent-of-code-2023/day1/input.txt");
+        Scanner sc = new Scanner(file);
+
+        int total = 0;
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            // line = line.replaceAll("[^0-9]", "");
+
+            int intLine = Integer.parseInt(line);
+            System.out.println(intLine);
+
+            String first = line.substring(0, 1);
+            String last = line.substring(line.length() - 1);
+
+            total += Integer.valueOf(first + last);
+        }
+
+        System.out.println(total);
+
+        sc.close();
+    }
+}
