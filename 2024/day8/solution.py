6a474527e93c6c8cc677cc10e9c4e2e117baed7e
Colin Wilson <github@colinwilson.dev>
2024-12-11 11:37:43 -0500

    feat: :sparkles: Finish day 8

--- 2024/day8/solution.py
+++ 2024/day8/solution.py
@@ -1,13 +1,50 @@
-import numpy as np
+class Antenna:
+    def __init__(self, row, col, symbol):
+        self.row = row
+        self.col = col
+        self.symbol = symbol
+    
+def is_in_bounds(row, col, instructions) -> bool:
+    return 0 <= row < len(instructions) and 0 <= col < len(instructions[0])
 
 def part1(instructions) -> int:
-    return 0
+    antennas = []
+    for row, line in enumerate(instructions):
+        for col, symbol in enumerate(line):
+            if symbol != ".":
+                antennas.append(Antenna(row, col, symbol))
+    
+    antinodes = []
+    for antenna1 in antennas:
+        for antenna2 in [antenna for antenna in antennas if antenna.symbol == antenna1.symbol and antenna != antenna1]:
+            check_row = antenna1.row + (antenna1.row - antenna2.row)
+            check_col = antenna1.col + (antenna1.col - antenna2.col)
+            if is_in_bounds(check_row, check_col, instructions):
+                antinodes.append((check_row, check_col))
+    
+    return len(set(antinodes))
 
 def part2(instructions) -> int:
-    return 0
+    antennas = []
+    for row, line in enumerate(instructions):
+        for col, symbol in enumerate(line):
+            if symbol != ".":
+                antennas.append(Antenna(row, col, symbol))
+    
+    antinodes = []
+    for antenna1 in antennas:
+        for antenna2 in [antenna for antenna in antennas if antenna.symbol == antenna1.symbol and antenna != antenna1]:
+            check_row = antenna1.row
+            check_col = antenna1.col
+            while is_in_bounds(check_row, check_col, instructions):
+                antinodes.append((check_row, check_col))
+                check_row += (antenna1.row - antenna2.row)
+                check_col += (antenna1.col - antenna2.col)
+    
+    return len(set(antinodes))
 
 with open("input.txt") as f:
-    board = f.readlines()
+    instructions = [line.strip() for line in f.readlines()]
 
-    print(part1(board))
-    print(part2(board))
+    print(part1(instructions))
+    print(part2(instructions))
