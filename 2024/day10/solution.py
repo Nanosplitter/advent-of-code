0705cbb633b1cb0f52c4952dff8b919efcebd6c6
Colin Wilson <github@colinwilson.dev>
2024-12-11 21:59:54 -0500

    feat: :sparkles: Finish day 9 and 10

--- 2024/day10/solution.py
+++ 2024/day10/solution.py
@@ -1,13 +1,53 @@
-import numpy as np
+def get_trail_score(head_row, head_col, map):
+    if map[head_row][head_col] == '9':
+        return {(head_row, head_col)}
+    else:
+        reachable_nines = set()
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        for direction in directions:
+            check_row = head_row + direction[0]
+            check_col = head_col + direction[1]
+            if 0 <= check_row < len(map) and 0 <= check_col < len(map[0]):
+                next_value = str(int(map[head_row][head_col]) + 1)
+                if map[check_row][check_col] == next_value:
+                    reachable_nines.update(get_trail_score(check_row, check_col, map))
+        return reachable_nines
+        
+def get_trail_rating(head_row, head_col, map):
+    if map[head_row][head_col] == '9':
+        return 1
+    else:
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        dir_ratings = 0
+        for direction in directions:
+            check_row = head_row + direction[0]
+            check_col = head_col + direction[1]
+            if 0 <= check_row < len(map) and 0 <= check_col < len(map[0]):
+                next_value = str(int(map[head_row][head_col]) + 1)
+                if map[check_row][check_col] == next_value:
+                    dir_ratings += get_trail_rating(check_row, check_col, map)
+        return dir_ratings
 
 def part1(instructions) -> int:
-    return 0
+    total_score = 0
+    for row_index, row in enumerate(instructions):
+        for col_index, col in enumerate(row):
+            if col == '0':
+                score = len(get_trail_score(row_index, col_index, instructions))
+                total_score += score
+    return total_score
 
 def part2(instructions) -> int:
-    return 0
+    total_rating = 0
+    for row_index, row in enumerate(instructions):
+        for col_index, col in enumerate(row):
+            if col == '0':
+                rating = get_trail_rating(row_index, col_index, instructions)
+                total_rating += rating
+    return total_rating
 
-with open("input.txt") as f:
-    board = f.readlines()
-
-    print(part1(board))
-    print(part2(board))
+with open("2024/day10/input.txt") as f:
+    instructions = [line.strip() for line in f.readlines()]
+    
+    print(part1(instructions))
+    print(part2(instructions))
--- 2024/day9/solution.py
+++ 2024/day9/solution.py
@@ -1,13 +1,77 @@
-import numpy as np
-
 def part1(instructions) -> int:
-    return 0
+    curr_id = 0
+    free_space = False
+    blocks = []
+    for num in instructions[0]:
+        count = int(num)
+        if free_space:
+            blocks.extend([None] * count)
+            free_space = False
+        else:
+            blocks.extend([curr_id] * count)
+            free_space = True
+            curr_id += 1
+
+    for i in range(len(blocks)):
+        if blocks[i] is None:
+            for j in range(len(blocks) - 1, i, -1):
+                if blocks[j] is not None:
+                    blocks[i], blocks[j] = blocks[j], None
+                    break
+
+    checksum = 0
+    for i, block in enumerate(blocks):
+        if block is not None:
+            checksum += block * i
+
+    return checksum
 
 def part2(instructions) -> int:
-    return 0
+    curr_id = 0
+    free_space = False
+    blocks = []
+    for num in instructions[0]:
+        count = int(num)
+        if free_space:
+            blocks.append([None] * count)
+            free_space = False
+        else:
+            blocks.append([curr_id] * count)
+            free_space = True
+            curr_id += 1
+    
+    for block_index in range(len(blocks))[::-1]:
+        if all(data is None for data in blocks[block_index]):
+            continue
+        for check_block_index in range(block_index):
+            if len([data for data in blocks[check_block_index] if data is None]) >= len(blocks[block_index]):
+                len_free_space = len(blocks[check_block_index])
+                len_data = len(blocks[block_index])
+                blocks[check_block_index] = [None] * (len_free_space - len(blocks[block_index]))
+                blocks.insert(check_block_index, blocks[block_index])
+                
+                blocks[block_index + 1] = [None] * len_data
+                break
+    
+    filesystem = []
+    
+    for block in blocks:
+        for data in block:
+            if data is not None:
+                filesystem.append(data)
+            else:
+                filesystem.append(None)
+    
+    checksum = 0
+    for i, data in enumerate(filesystem):
+        if data is not None:
+            checksum += data * i
+        
+    
+    return checksum
 
-with open("input.txt") as f:
-    board = f.readlines()
+with open("2024/day9/input.txt") as f:
+    instructions = f.readlines()
 
-    print(part1(board))
-    print(part2(board))
+    print(part1(instructions))
+    print(part2(instructions))
