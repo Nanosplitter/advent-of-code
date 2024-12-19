9b89b3bf094da946b2826c7945471bff09227970
Colin Wilson <github@colinwilson.dev>
2024-12-19 11:17:09 -0500

    feat: :sparkles: Finish day 19

--- 2024/day19/solution.py
+++ 2024/day19/solution.py
@@ -1,18 +1,63 @@
+from collections import defaultdict
+import os
 import sys
+import time
+from termcolor import colored
+
+cache = {'': 1}
+
+def color_towel(towel):
+    res = ""
+    for color in towel:
+        if color == "w":
+            res += colored("   ", 'black', 'on_white')
+        elif color == "r":
+            res += colored("   ", 'white', 'on_red')
+        elif color == "b":
+            res += colored("   ", 'white', 'on_black')
+        elif color == "g":
+            res += colored("   ", 'white', 'on_green')
+        elif color == "u":
+            res += colored("   ", 'white', 'on_blue')
+    return res
+
+def num_ways_to_make(design, towels):
     
-def part1(instructions) -> int:
-    return 0
+    if design not in cache:
+        cache[design] = sum(num_ways_to_make(design[len(towel):], towels) for towel in towels if design.startswith(towel))
 
-def part2(instructions) -> int:
-    return 0
+    return cache[design]
 
-if len(sys.argv) < 2:
-    sys.exit(1)
+def part1(towels, designs, visualize=False) -> str:
+    
+    matches_found = 0
+        
+    for design in designs:
+        if visualize:
+            os.system('cls||clear')
+            print(f"Towels:")
+            for towel in towels:
+                print(color_towel(towel))
+            print(f"Design: {color_towel(design)}")
+            time.sleep(0.5)
+        ways_to_make = num_ways_to_make(design, towels)
+        matches_found += 1 if ways_to_make > 0 else 0
+    
+    return matches_found
+
+
+def part2(designs, visualize=False) -> int:
+    return sum(cache[design] for design in designs)
 
 input_file = sys.argv[1]
 
 with open(input_file) as f:
-    instructions = f.readlines()
+    lines = f.readlines()
+    
+    towels = tuple([line.strip() for line in lines[0].split(', ')])
+    
+    designs = [line.strip() for line in lines[2:]]
+    
+    print(part1(towels, designs, visualize=False))
+    print(part2(designs, visualize=False))
 
-    print(part1(instructions))
-    print(part2(instructions))
