f8ea7f1a06724e1eebb81a625ba9a60516fb828f
Colin Wilson <github@colinwilson.dev>
2024-12-19 13:55:11 -0500

    feat: :sparkles: Add a visualization for part 1

--- 2024/day19/solution.py
+++ 2024/day19/solution.py
@@ -22,10 +22,19 @@ def color_towel(towel):
     return res
 
 def num_ways_to_make(design, towels):
+    if not design:
+        return 1, []
     
     if design not in cache:
-        cache[design] = sum(num_ways_to_make(design[len(towel):], towels) for towel in towels if design.startswith(towel))
-
+        total, example = 0, []
+        for towel in towels:
+            if design.startswith(towel):
+                ways, sol = num_ways_to_make(design[len(towel):], towels)
+                total += ways
+                if not example and sol is not None:
+                    example = [towel] + sol
+        cache[design] = (total, example)
+    
     return cache[design]
 
 def part1(towels, designs, visualize=False) -> str:
@@ -33,21 +42,30 @@ def part1(towels, designs, visualize=False) -> str:
     matches_found = 0
         
     for design in designs:
+        
+        ways_to_make = num_ways_to_make(design, towels)
+        matches_found += 1 if ways_to_make[0] > 0 else 0
+        
         if visualize:
             os.system('cls||clear')
-            print(f"Towels:")
-            for towel in towels:
-                print(color_towel(towel))
-            print(f"Design: {color_towel(design)}")
+            print("="*300)
+            print(f"Design:                {color_towel(design)}")
             time.sleep(0.5)
-        ways_to_make = num_ways_to_make(design, towels)
-        matches_found += 1 if ways_to_make > 0 else 0
+            if ways_to_make[0] == 0:
+                os.system('cls||clear')
+                print("="*300)
+                print(f"No solution possible!: {color_towel(design)}")
+            else:
+                os.system('cls||clear')
+                print("="*300)
+                print(f"Solution:              {"|".join([color_towel(towel) for towel in ways_to_make[1]])}")
+            time.sleep(1)
     
     return matches_found
 
 
 def part2(designs, visualize=False) -> int:
-    return sum(cache[design] for design in designs)
+    return sum(cache[design][0] for design in designs)
 
 input_file = sys.argv[1]
 
