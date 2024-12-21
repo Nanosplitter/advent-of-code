a65af1ab3da25a5a1e64cbc1cae65995eed894b3
Colin Wilson <github@colinwilson.dev>
2024-12-21 14:25:00 -0500

    feat: :sparkles: Finish day 21

--- 2024/day21/solution.py
+++ 2024/day21/solution.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 from functools import cache, lru_cache
+from itertools import product
 import sys
 
 N_PAD = {
@@ -23,70 +25,66 @@ D_PAD = {
 }
 PADS = [N_PAD] + [D_PAD] * 24
 
-@lru_cache
+num_paths_cache = defaultdict(None)
+key_paths_cache = defaultdict(None)
+
+@cache
 def bfs(pad_type, start, target, visited=(), path=()):
     if start == target:
-        #print("Found target", target)
-        return path
+        return [path]
     if start in visited:
-        return None
+        return []
     
-    check_paths = []
+    all_paths = []
     for next, dir in PADS[pad_type][start]:
-        check_paths.append(bfs(pad_type, next, target, visited + (start,), path + (dir,)))
-    if any(check_paths):
-        valid_paths = [cp for cp in check_paths if cp]
-        return min(valid_paths, key=lambda p: sum(1 for i in range(1, len(p)) if p[i] != p[i-1])) if valid_paths else None
-
-    return None
-
-# def get_path(code, robot_num):
+        new_paths = bfs(pad_type, next, target, visited + (start,), path + (dir,))
+        if new_paths:
+            all_paths.extend(new_paths)
     
+    if all_paths:
+        min_turns = min(sum(1 for i in range(1, len(p)) if p[i] != p[i-1]) for p in all_paths)
+        min_turn_paths = [p for p in all_paths if sum(1 for i in range(1, len(p)) if p[i] != p[i-1]) == min_turns]
+        return min_turn_paths
 
+    return []
 
-def get_keypad_path(code, num_robots):
-    counter = 0
-    print("Code", code)
-    code = "A" + code
-    for robot_num in range(num_robots):
-        print(f"Robot {robot_num}")
-        code_path = []
-        for num in range(len(code) - 1):
-            
-            print(f"Finding path from {code[num]} to {code[num + 1]}")
-            path = bfs(robot_num, code[num], code[num + 1])
-            print("got path")
-            #print("Path", path)
-            code_path.extend(path)
-            code_path.append("A")
-        
-        #print(code_path)
-        code = ["A"] + code_path[:]
+@cache
+def get_path(code, robot_type):
+    code_path = []
+    for pair in zip(code, code[1:]):
+        if robot_type == 0:
+            path = num_paths_cache[pair]
+        else:
+            path = key_paths_cache[pair]
+
+        code_path.extend(path)
+        code_path.append("A")
     return code_path
-        
-    
-def part1(instructions):
-    codes = [code.strip() for code in instructions]
-    total_complexity = 0
-    
-    for code in codes:
-        path = get_keypad_path(code, 3)
-        print(f"Path length for code {code} is {len(path)}")
-        total_complexity += len(path) * int(code[:-1])
-        
-    return total_complexity
-    
 
-def part2(instructions):
-    codes = [code.strip() for code in instructions]
-    total_complexity = 0
+@cache
+def get_keypad_len(code, num_robots, robot_type=0):
+    if num_robots == 0:
+        return len(code)
     
-    for code in codes:
-        path = get_keypad_path(code, 25)
-        print(f"Path length for code {code} is {len(path)}")
-        total_complexity += len(path) * int(code[:-1])
-        
-    return total_complexity
+    total_moves = 0
+    for pair in zip(('A',) + code, code):
+        if robot_type == 0:
+            paths = num_paths_cache[pair]
+        else:
+            paths = key_paths_cache[pair]
+
+        total_moves += min(get_keypad_len(path + ('A',), num_robots - 1, 1) for path in paths)
+    
+    return total_moves
+
+def get_complexity(codes, num_robots):
+    return sum(get_keypad_len(tuple(list(code)), num_robots + 1) * int(code[:-1]) for code in codes)
+
+def part1(codes):
+    return get_complexity(codes, 2)
+    
+def part2(codes):
+    return get_complexity(codes, 25)
 
 if len(sys.argv) < 2:
     sys.exit(1)
@@ -95,6 +93,15 @@ input_file = sys.argv[1]
 
 with open(input_file) as f:
     instructions = f.readlines()
+    
+    num_pad_combos = product("0123456789A", repeat=2)
+    for combo in num_pad_combos:
+        num_paths_cache[combo] = bfs(0, combo[0], combo[1])
+        
+    key_pad_combos = product("<^>vA", repeat=2)
+    for combo in key_pad_combos:
+        key_paths_cache[combo] = bfs(1, combo[0], combo[1])
 
-    print(part1(instructions))
-    #print(part2(instructions))
+    codes = [code.strip() for code in instructions]
+    print(part1(codes))
+    print(part2(codes))
