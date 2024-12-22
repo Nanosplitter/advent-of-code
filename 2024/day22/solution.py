14056ad8cfe37810d613720dab2345388d114320
Colin Wilson <github@colinwilson.dev>
2024-12-22 01:47:55 -0500

    feat: :sparkles: Finish day 22

--- 2024/day22/solution.py
+++ 2024/day22/solution.py
@@ -1,10 +1,61 @@
 import sys
-    
-def part1(instructions) -> int:
-    return 0
 
-def part2(instructions) -> int:
-    return 0
+def mix(sn, b):
+    return sn ^ b
+
+def prune(sn):
+    return sn % 16777216
+
+def get_windows(lst, n):
+    return list(map(tuple, zip(*[lst[i:] for i in range(n)])))
+
+def get_next(sn):
+    sn = mix(sn, sn * 64)
+    sn = prune(sn)
+    
+    sn = mix(sn, sn // 32)
+    sn = prune(sn)
+    
+    sn = mix(sn, sn * 2048)
+    sn = prune(sn)
+    
+    return sn
+    
+def part1(nums) -> int:
+    total = 0
+    for num in nums:
+        for i in range(2000):
+            num = get_next(num)
+        total += num
+    
+    return total
+
+def part2(nums) -> int:
+    
+    windows = {}
+    for num in nums:
+        last_price = num % 10
+        price_deltas = []
+        buyer_windows = set()
+        for i in range(2000):
+            new_num = get_next(num)
+            price = new_num % 10
+            price_deltas.append((price - last_price))
+            num = new_num
+            if i > 4:
+                last_window = tuple(price_deltas[-4:])
+                if last_window not in buyer_windows:
+                    buyer_windows.add(last_window)
+                    
+                    if last_window not in windows:
+                        windows[last_window] = 0
+                    
+                    windows[last_window] += price
+            last_price = price
+    
+    windows = sorted(windows.items(), key=lambda x: x[1])
+    
+    return windows[-1][1]
 
 if len(sys.argv) < 2:
     sys.exit(1)
@@ -12,7 +63,7 @@ if len(sys.argv) < 2:
 input_file = sys.argv[1]
 
 with open(input_file) as f:
-    instructions = f.readlines()
+    nums = [int(n) for n in f.readlines()]
 
-    print(part1(instructions))
-    print(part2(instructions))
+    print(part1(nums))
+    print(part2(nums))
