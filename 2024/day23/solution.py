484a450a6d9df7dc9a1510c57fd4257bed977362
Colin Wilson <github@colinwilson.dev>
2024-12-23 01:29:35 -0500

    feat: :sparkles: Finish day23

--- 2024/day23/solution.py
+++ 2024/day23/solution.py
@@ -1,10 +1,46 @@
+from collections import defaultdict
+from itertools import product
+from networkx.algorithms.clique import find_cliques as maximal_cliques
+import networkx as nx
 import sys
-    
-def part1(instructions) -> int:
-    return 0
 
-def part2(instructions) -> int:
-    return 0
+connections = defaultdict(list)
+
+connected_pairs = defaultdict(bool)
+    
+
+def is_interconnected(group):
+    for connection in group:
+        for other_connection in group:
+            if connection == other_connection:
+                continue
+            if other_connection not in connections[connection]:
+                return False
+    return True
+
+def get_interconnected_groups(group_size):
+    groups = set()
+    for connection in connections:
+        
+        pairs = product(connections[connection], repeat=group_size - 1)
+        for pair in pairs:
+            if list(set(pair)) != list(pair):
+                continue
+            
+            if is_interconnected(pair + (connection,)):
+                groups.add(tuple(sorted(pair + (connection,))))
+
+    return list(groups)
+
+def part1() -> int:
+    return sum([1 for clique in get_interconnected_groups(3) if any([node.startswith("t") for node in clique])])
+
+def part2() -> tuple[int, set]:
+    graph = nx.Graph(dict(connections))
+
+    all_cliques = sorted((clique for clique in maximal_cliques(graph)), key=len, reverse=True)
+    
+    return ",".join(sorted(all_cliques[0]))
 
 if len(sys.argv) < 2:
     sys.exit(1)
@@ -13,6 +49,15 @@ input_file = sys.argv[1]
 
 with open(input_file) as f:
     instructions = f.readlines()
+    
+    for connection in instructions:
+        connection = connection.strip().split("-")
+        connections[connection[0]].append(connection[1])
+        connections[connection[1]].append(connection[0])
 
-    print(part1(instructions))
-    print(part2(instructions))
+    for key in connections:
+        connections[key].sort()
+    
+    
+    print(part1())
+    print(part2())
