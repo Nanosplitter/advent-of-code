e065383fdf1e46eee239281665b35ba767e0dc6b
Colin Wilson <github@colinwilson.dev>
2025-12-02 18:42:17 -0500

    feat(2025_day02): :sparkles: finish part 2

--- 2025/day02/solution.py
+++ 2025/day02/solution.py
@@ -3,9 +3,10 @@ Advent of Code 2025 - Day 2
 """
 
 import sys
+from functools import cache
 
 sys.path.insert(0, str(__file__).split("2025")[0])
-from libraries.aoc_runner import run, get_input
+from libraries.aoc_runner import run
 
 
 def parse(lines: list[str]):
@@ -33,7 +34,6 @@ def part1(data) -> int:
 
             if len(check_str) % 2 == 0:
                 if range[0] <= int(half + half) <= range[1]:
-                    print("Adding invalid ID:", half + half)
                     invalid_ids.append(int(half + half))
             if half == "":
                 half = "0"
@@ -42,9 +42,53 @@ def part1(data) -> int:
     return sum(invalid_ids)
 
 
+@cache
+def check_invalid(fruit: str, range_start: int, range_end: int) -> list[int]:
+    invalid_ids = []
+
+    for seed in generate_seeds(fruit):
+        check = int(seed)
+
+        while check <= range_end:
+            if check >= range_start:
+                invalid_ids.append(check)
+
+            check = int(str(check) + seed)
+    return invalid_ids
+
+
+@cache
+def generate_seeds(fruit: str) -> list[str]:
+    seeds = []
+
+    for i in range(len(fruit)):
+        seeds.append(fruit[: i + 1])
+
+    return seeds
+
+
 def part2(data) -> int:
-    """Solve part 2."""
-    return 0
+    invalid_ids = []
+
+    for low, high in data:
+        low_str = str(low)
+        high_str = str(high)
+        low_half = low_str[: (len(low_str) // 2)]
+        if low_half == "":
+            low_half = 1
+        high_half = high_str[: (len(high_str) // 2) + (len(high_str) % 2 != 0)]
+
+        range_invalid = []
+
+        for fruit in range(int(low_half), int(high_half) + 1):
+            invalid = list(set(check_invalid(str(fruit), low, high)))
+
+            range_invalid += invalid
+
+        range_invalid = list(set(range_invalid))
+        invalid_ids += range_invalid
+
+    return sum(invalid_ids)
 
 
 if __name__ == "__main__":
